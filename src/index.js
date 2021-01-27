cb0cd7903774ac8333ddaf5e00f11e08a5454940
Rafael Dona <rafa_dona@hotmail.com>
2021-01-27 17:45:36 -0300

    starting commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import ReactDOM from "react-dom";
+import animals from "./data";
+
+
+const [cat, dog] = animals;
+
+const {name:catName, sound:catSound} = cat;
+
+console.log(cat)
+console.log(dog)
+
+console.log(catSound)
+
+// ReactDOM.render(
+//   <table>
+//     <tr>
+//       <th>Brand</th>
+//       <th>Top Speed</th>
+//     </tr>
+//     <tr>
+//       <td>{tesla.model}</td>
+//       <td>{teslaTopSpeed}</td>
+//       <td>{teslaTopColour}</td>
+//     </tr>
+//     <tr>
+//       <td>{honda.model}</td>
+//       <td>{hondaTopSpeed}</td>
+//       <td>{hondaTopColour}</td>
+//     </tr>
+//   </table>,
+//   document.getElementById("root")
+// );
